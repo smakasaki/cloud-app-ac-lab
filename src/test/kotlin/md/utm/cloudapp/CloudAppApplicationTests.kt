d1796e8b0c0f6e26a421957f5170f8227e9ad2b5
Andrei G <salamandr258@gmail.com>
2026-05-26 20:49:50 +0300

    fix: use test profile in CloudAppApplicationTests

--- src/test/kotlin/md/utm/cloudapp/CloudAppApplicationTests.kt
+++ src/test/kotlin/md/utm/cloudapp/CloudAppApplicationTests.kt
@@ -2,8 +2,10 @@ package md.utm.cloudapp
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CloudAppApplicationTests {
 
     @Test
