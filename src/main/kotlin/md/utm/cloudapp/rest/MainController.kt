04ac1dfe61a54c66a2581180a9a4527c436c52b0
Andrei G <salamandr258@gmail.com>
2026-05-28 16:25:21 +0300

    feat: enhance logging in QuoteController and MainController

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -1,15 +1,21 @@
 package md.utm.cloudapp.rest
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MainController {
 
+    private val log = LoggerFactory.getLogger(MainController::class.java)
+
     @GetMapping("/")
-    fun root(): Map<String, String> = mapOf(
-        "app" to "cloud-app",
-        "status" to "ok",
-        "endpoints" to "/quotes, /quotes/{id}, /quotes/random, /quotes/author/{author}, /quotes/stats"
-    )
+    fun root(): Map<String, String> {
+        log.debug("GET / -> root endpoint accessed")
+        return mapOf(
+            "app" to "cloud-app",
+            "status" to "ok",
+            "endpoints" to "/quotes, /quotes/{id}, /quotes/random, /quotes/author/{author}, /quotes/stats"
+        )
+    }
 }
